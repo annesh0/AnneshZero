81b8c26e63b828e1e73a5ad1186c83386fe34f3b
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-05-08 22:53:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var config = {
 }
 
 function onDragStart(source, piece, position, orientation) {
+    console.log(whiteRookValues)
     //make sure game not over
     if(chess.game_over()) return false
     //make sure white to move and only picking up white pieces
@@ -24,7 +25,7 @@ function onDrop(source, target) {
     })
     if (move == null) return 'snapback'
 
-    window.setTimeout(simpleEvalAI, 500)
+    window.setTimeout(minimaxAI, 500)
 }
 const generalPieceValue = new Map([
     ["p", 10],
@@ -35,7 +36,7 @@ const generalPieceValue = new Map([
     ["k", 900]
 ]);
 
-const whitePawnValues = [
+var whitePawnValues = [
     [0,  0,  0,  0,  0,  0,  0,  0],
     [50, 50, 50, 50, 50, 50, 50, 50],
     [10, 10, 20, 30, 30, 20, 10, 10],
@@ -44,9 +45,9 @@ const whitePawnValues = [
     [5, -5,-10,  0,  0,-10, -5,  5],
     [5, 10, 10,-20,-20, 10, 10,  5],
     [0,  0,  0,  0,  0,  0,  0,  0]
-]
+];
 
-const whiteKnightValues = [
+var whiteKnightValues = [
     [-50,-40,-30,-30,-30,-30,-40,-50],
     [-40,-20,  0,  0,  0,  0,-20,-40],
     [-30,  0, 10, 15, 15, 10,  0,-30],
@@ -55,10 +56,10 @@ const whiteKnightValues = [
     [-30,  5, 10, 15, 15, 10,  5,-30],
     [-40,-20,  0,  5,  5,  0,-20,-40],
     [-50,-40,-30,-30,-30,-30,-40,-50],
-]
+];
 
-const whiteBishopValues = [
-    [-20,-10,-10,-10,-10,-10,-10,-20]
+var whiteBishopValues = [
+    [-20,-10,-10,-10,-10,-10,-10,-20],
     [-10,  0,  0,  0,  0,  0,  0,-10],
     [-10,  0,  5, 10, 10,  5,  0,-10],
     [-10,  5,  5, 10, 10,  5,  5,-10],
@@ -66,10 +67,10 @@ const whiteBishopValues = [
     [-10, 10, 10, 10, 10, 10, 10,-10],
     [-10,  5,  0,  0,  0,  0,  5,-10],
     [-20,-10,-10,-10,-10,-10,-10,-20,]
-]
+];
 
-const whiteRookValues = [
-    [0,  0,  0,  0,  0,  0,  0,  0]
+var whiteRookValues = [
+    [0,  0,  0,  0,  0,  0,  0,  0],
     [5, 10, 10, 10, 10, 10, 10,  5],
     [-5,  0,  0,  0,  0,  0,  0, -5],
     [-5,  0,  0,  0,  0,  0,  0, -5],
@@ -77,9 +78,9 @@ const whiteRookValues = [
     [-5,  0,  0,  0,  0,  0,  0, -5],
     [-5,  0,  0,  0,  0,  0,  0, -5],
     [0,  0,  0,  5,  5,  0,  0,  0]
-]
+];
 
-const whiteQueenValues = [
+var whiteQueenValues = [
     [-20,-10,-10, -5, -5,-10,-10,-20],
     [-10,  0,  0,  0,  0,  0,  0,-10],
     [-10,  0,  5,  5,  5,  5,  0,-10],
@@ -88,9 +89,9 @@ const whiteQueenValues = [
     [-10,  5,  5,  5,  5,  5,  0,-10],
     [-10,  0,  5,  0,  0,  0,  0,-10],
     [-20,-10,-10, -5, -5,-10,-10,-20]
-]
+];
 
-const whiteKingValues = [
+var whiteKingValues = [
     [-30,-40,-40,-50,-50,-40,-40,-30],
     [-30,-40,-40,-50,-50,-40,-40,-30],
     [-30,-40,-40,-50,-50,-40,-40,-30],
@@ -99,15 +100,14 @@ const whiteKingValues = [
     [-10,-20,-20,-20,-20,-20,-20,-10],
     [20, 20,  0,  0,  0,  0, 20, 20],
     [20, 30, 10,  0,  0, 10, 30, 20]
-]
-
+];
 //mirror values for black pieces
-blackPawnValues = whitePawnValues.slice().reverse()
-blackKnightValues = whiteKnightValues.slice().reverse()
-blackBishopValues = whiteBishopValues.slice().reverse()
-blackRookValues = whiteRookValues.slice().reverse()
-blackQueenValues = whiteQueenValues.slice().reverse()
-blackKingValues = whiteKingValues.slice().reverse()
+var blackPawnValues = whitePawnValues.slice().reverse()
+var blackKnightValues = whiteKnightValues.slice().reverse()
+var blackBishopValues = whiteBishopValues.slice().reverse()
+var blackRookValues = whiteRookValues.slice().reverse()
+var blackQueenValues = whiteQueenValues.slice().reverse()
+var blackKingValues = whiteKingValues.slice().reverse()
 
 function updatedPosEval(position) {
     var value = 0
@@ -151,6 +151,7 @@ function updatedPosEval(position) {
                             value += whiteBishopValues[i][j];
                             break;
                         case "r":
+                            console.log(whiteRookValues[0][0])
                             value += whiteRookValues[i][j];
                             break;
                         case "q":
@@ -212,7 +213,7 @@ function minimax(chess, depth, flag) {
         return [bestMove, maxEval]
     } else {
         var minEval = 9999
-        for(const move of chess.move()) {
+        for(const move of chess.moves()) {
             chess.move(move)
             var curVal = Math.min(minEval, minimax(chess,depth-1,!flag)[1])
             chess.undo()
@@ -229,7 +230,7 @@ function minimax(chess, depth, flag) {
 function minimaxAI() {
     if(chess.game_over()) return
     if(chess.turn() == 'b') {
-        chess.move(minimax(chess, 5, false)[0])
+        chess.move(minimax(chess, 3, true)[0])
         board1.position(chess.fen())
     }
 }
@@ -271,5 +272,5 @@ function randomAI() {
 }
 var board1 = Chessboard('board1', config)
 
-window.setTimeout(simpleEvalAI, 500)
+window.setTimeout(minimaxAI, 500)
 
