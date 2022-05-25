e16118065f9f07aeca921213238b613b9058d398
annesh0 <67394141+annesh0@users.noreply.github.com>
2022-05-24 20:20:53 -0700

    implemented model

--- chess.py
+++ chess.py
@@ -1,4 +1,78 @@
+import os
+from typing import OrderedDict
 import torch
-import sqlite3
+from torch.utils.data import Dataset, DataLoader, IterableDataset, random_split
+from peewee import *
+from torch import nn
+import torch.nn.functional as F
+import base64
+import numpy as np
+import pytorch_lightning as pl
 
-db = sqlite3.connect("2021-07-31-lichess-evaluations-37MM.db.gz")
+
+db = SqliteDatabase("2021-07-31-lichess-evaluations-37MM.db")
+
+class Evaluations(Model):
+    id = IntegerField()
+    fen = TextField()
+    binary = BlobField()
+    eval = FloatField()
+
+    class Meta:
+        database = db
+    def binary_base64(self):
+        return base64.b64encode(self.binary)
+
+db.connect()
+
+eval = Evaluations.get(Evaluations.id == 1)
+print(eval.fen)
+
+class EvalutionDataset(IterableDataset):
+    def __init__(self, count):
+        self.count = count
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return self[random.randrange(0, self.count)]
+    def __len__(self):
+        return self.count
+    def __getitem__(self, index):
+        eval = Evaluations.get(Evaluations.id == index+1)
+        bin = np.frombuffer(eval.binary, dtype=np.unit8)
+        bin = np.unpackbits(bin, axis=0).astype(np.single)
+        eval.eval = max(eval.eval, -15)
+        eval.eval = min(eval.eval, 15)
+        ev = np.array([eval.eval]).astype(np.single)
+        return {"binary":bin, "eval":ev}
+
+class Model(pl.LightningModule):
+    def __init__(self, learning_rate=1e-3,batch_size=1024,layer_count=10):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.layer_count = layer_count
+        layers = []
+        for i in range(layer_count - 1):
+            layers.append((f"linear-{i}", nn.Linear(808,808)))
+            layers.append((f"relu-{i}", nn.ReLU()))
+        layers.append((f"linear-{layer_count-1}", nn.Linear(808,1)))
+        self.seq = nn.Sequential(OrderedDict(layers))
+    
+    def forward(self, x):
+        return self.seq(x)
+    
+    def training_step(self, batch, batch_index):
+        x = batch["binary"]
+        y = batch["eval"]
+        y_hat = self(x)
+        loss = F.li_loss(y_hat, y)
+        self.log("training_loss", loss)
+        return loss
+    
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(),lr=self.learning_rate)
+
+    def train_dataloader(self):
+        dataset = EvalutionDataset(count=37164639)
+        return DataLoader(dataset, batch_size=self.batch_size, num_workers=2,pin_memory=True)
